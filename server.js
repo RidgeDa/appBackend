d187d6812260834d57ec2abc3bc6c05a38a95043
Ridge Dampare <ridge_d@hotmail.com>
2020-02-08 18:00:44 +0000

    added users to mongoDB

--- server.js
+++ server.js
@@ -33,6 +33,13 @@ app.listen(3400);
 const express = require('express') 
 const app = express()
 
+const lessons = [
+  { 'topic': 'math', 'location': 'hendon', 'price': 100 },
+  { 'topic': 'math', 'location': 'colindale', 'price': 80 },
+  { 'topic': 'math', 'location': 'brent cross', 'price': 90 },
+  { 'topic': 'math', 'location': 'golders green', 'price': 120 },
+  ];
+
 // Load bodyparser module for json
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -44,8 +51,20 @@ MongoClient.connect('mongodb://localhost:27017/', (err, client) => {
   db = client.db('afterschool') 
 })
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //Get the collection name
 app.param('signUp', (req, res, next, collectionName) => {   
+  req.collection = db.collection(collectionName)   
+  return next() 
+})
+
+//Get the collection name
+app.param('login', (req, res, next, collectionName) => {   
   req.collection = db.collection(collectionName)   
   return next() 
 })
@@ -56,6 +75,16 @@ app.get('/', function (req, res) {
   res.send('Select a collection, e.g., /collections/signUp') 
 })
 
+// dispaly a message for root path to show that API is working 
+app.get('/', function (req, res) {     
+  res.send('Select a collection, e.g., /collections/login') 
+})
+
+// dispaly a message for root path to show that API is working 
+app.get('/', function (req, res) {     
+  res.send('Select a collection, e.g., /collections/lessons') 
+})
+
 // retrieve all the objects from an collection 
 app.get('/collections/:signUp', (req, res) => {     
   req.collection.find({}, { limit: 5, sort: [['price', -1]] }).toArray((e, results) => {         
@@ -64,14 +93,48 @@ app.get('/collections/:signUp', (req, res) => {
   }) 
 })
 
-// add a lesson
-app.post('/signup/:collectionName', (req, res, next) => {     
+// retrieve all the objects from an collection 
+app.get('/collections/:login', (req, res) => {     
+  req.collection.find({}, { limit: 5, sort: [['price', -1]] }).toArray((e, results) => {         
+    if (e) return next(e)         
+    res.send(results)     
+  }) 
+})
+
+// retrieve all the objects from an collection 
+app.get('/collections/:lessons', (req, res) => {     
+  req.collection.find({}, { limit: 5, sort: [['price', -1]] }).toArray((e, results) => {         
+    if (e) return next(e)         
+    res.send(results)     
+  }) 
+})
+
+// add a signUp
+app.post('/:signUp', (req, res, next) => {     
   // TODO: Validate req.body
        req.collection.insert(req.body, (e, results) => {         
          if (e) return next(e)         
          res.send(results.ops)     
         }) 
       })
+
+// add a lessons
+app.post('/:login', (req, res, next) => {     
+  // TODO: Validate req.body
+       req.collection.insert(req.body, (e, results) => {         
+         if (e) return next(e)         
+         res.send(results.ops)     
+        }) 
+      })
+
+// add a lessons
+app.post('/:lessons', (req, res, next) => {     
+  // TODO: Validate req.body
+       req.collection.insert(req.body, (e, results) => {         
+         if (e) return next(e)         
+         res.send(results.ops)     
+        }) 
+      })      
 
 // retrieve a lesson by mongodb ID
 const ObjectID = require('mongodb').ObjectID; 
@@ -82,6 +145,11 @@ app.get('/collections/:collectionName/:id', (req, res, next) => {
     res.send(result)     
   }) 
 })
+
+// define the route
+app.get('/:lessons', (req, res) => {
+  res.send(JSON.stringify(lessons));
+  })
 
 // update a lesson by ID
  app.put('/collections/:collectionName/:id', (req, res, next) => {     
